ba7ae5d92595b07c8f23678275fc52b05dbb6365
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-20 09:26:35 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 24
+second_number = 24.24
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
